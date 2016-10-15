f0e53cfe433922dc3011d0c633332d19af81428a
Adityo Rancaka <cakdityo@gmail.com>
2016-10-15 14:43:46 +0700

    React is awesomeness!

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        <h1>Welcome to React boilerplate!</h1>
+        <p><small>by Adityo Rancaka</small></p>
+    </div>,
+    document.getElementById('app')
+)
